258e8b8c6786de41ef7ca14248268024e9fbe5d9
Keito Osaki <keito-osaki@freee.co.jp>
2023-04-23 03:00:14 +0900

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.colors = {
+  cursor_bg = '#999999',
+}
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+config.cursor_blink_rate = 500
+config.debug_key_events = true
+config.default_cursor_style = 'BlinkingBlock'
+config.font = wezterm.font 'Hack Nerd Font Mono'
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x1bf',
+  },
+  {
+    key = 'b',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x1bb',
+  },
+  {
+    key = 'x',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x1bx',
+  },
+}
+config.line_height = 1.1
+config.window_decorations = 'RESIZE'
+config.window_frame = {
+  active_titlebar_bg = '#000000',
+}
+config.window_padding = {
+  top = 1,
+  right = 1,
+  left = 1,
+  bottom = 1,
+}
+
+return config
