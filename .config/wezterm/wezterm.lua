64b71350647b77bdf93086afb3efb014e2886049
kate <kate@kate-workstation>
2023-07-09 08:35:37 +0900

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,8 +9,11 @@ config.cursor_blink_ease_out = 'Constant'
 config.cursor_blink_rate = 500
 config.debug_key_events = true
 config.default_cursor_style = 'BlinkingBlock'
-config.font = wezterm.font 'Hack Nerd Font Mono'
 config.enable_scroll_bar = false
+config.font = wezterm.font_with_fallback({
+  { family = 'Hack Nerd Font' },
+  { family = 'Hack Nerd Font', assume_emoji_presentation = true },
+})
 config.keys = {
   {
     key = 'f',
