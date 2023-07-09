e4cb2913f25ed128cc6d67d496fd949dc4792de4
kate <kate@pop-os.localdomain>
2023-07-09 14:08:20 +0900

    remove hook script to start emacs daemon

--- hooks/after_link.sh
+++ hooks/after_link.sh
@@ -2,10 +2,4 @@
 
 set -eu
 
-# enable Emacs daemon systemd unit
-systemctl enable --user emacs
-
-# restore guake preferences from linked file
-guake --restore-preferences=$HOME/.config/guake/guake.conf
-
 echo 'Finish!'
