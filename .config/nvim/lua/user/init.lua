c7d735f656c5b739252e95e451dd925e5b9a5cfe
kate <kate@pop-os.localdomain>
2023-07-15 20:51:40 +0900

    update wezterm config

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,8 +1,15 @@
+local buffer = require('astronvim.utils.buffer')
+
+function nav_buffer(direction)
+  buffer.nav(direction * (vim.v.count > 0 and vim.v.count or 1))
+end
+
 return {
   mappings = {
     n = {
-      ['<tab>'] = { ':bnext<CR>', desc = 'Next buffer' },
-      ['<S-tab>'] = { ':bprevious<CR>', desc = 'Prev buffer' },
+      ['<tab>'] = { function() nav_buffer(1) end, desc = 'Next tab' },
+      ['<S-tab>'] = { function() nav_buffer(-1) end, desc = 'Prev tab' },
+      ['<C-9>'] = { '<cmd>ToggleTerm<cr>', desc = 'Toggle terminal' },
     },
   },
 }
