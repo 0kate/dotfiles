5170d17d02d5b86e8e147388469eb591b2550748
keito-osaki <keito_osaki@techfund.jp>
2024-04-04 19:29:43 +0900

    update dotfiles

--- hooks/before_link.sh
+++ hooks/before_link.sh
@@ -3,8 +3,8 @@
 set -eu
 
 echo 'Prepare dependencies...'
-source ../scripts/apt-install.sh
-source ../scripts/build-neovim.sh
+# source ../scripts/apt-install.sh
+# source ../scripts/build-neovim.sh
 echo 'Done.'
 
 echo 'Start linking my dotfiles...'
